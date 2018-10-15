abb916fb152cbb93a2e810e4b7863a3b2379ffb7
Zhichao(Todd) Pan <pan.z@husky.neu.edu>
2018-10-14 20:45:42 -0400

    feat: put interface to sc

--- server/src/main/java/edu/neu/csye6200/LABDayCare/StudentsController.java
+++ server/src/main/java/edu/neu/csye6200/LABDayCare/StudentsController.java
@@ -16,4 +16,16 @@ public class StudentsController {
     fa.open();
     return new ArrayList<Student>(fa.lists().values());
   }
+
+
+  @PostMapping("/addRegistration")
+  public void addRegistration(@RequestParam("sid") String sid, @RequestParam("month") int m, @RequestParam("day") int d, @RequestParam("year") int y) {
+    fa.getById(sid).registration(m,d,y);;
+  }
+
+  @PostMapping("/addImmunization")
+  public void addImmunization(@RequestParam("sid") String sid, @RequestParam("type") String str,@RequestParam("month") int m, @RequestParam("day") int d, @RequestParam("year") int y) {
+    fa.getById(sid).immunization(str,m,d,y);;
+  }
+
 }
