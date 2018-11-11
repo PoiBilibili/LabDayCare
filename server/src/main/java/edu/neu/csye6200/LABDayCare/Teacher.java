0c0f54e04c9fd2d5231af393f05d59527d3aba7d
JingZhou <zhouruge@163.com>
2018-11-11 14:05:47 -0500

    add teachers member in classroom

--- server/src/main/java/edu/neu/csye6200/LABDayCare/Teacher.java
+++ server/src/main/java/edu/neu/csye6200/LABDayCare/Teacher.java
@@ -3,7 +3,7 @@ package edu.neu.csye6200.LABDayCare;
 public class Teacher extends Person implements Comparable<Teacher> {
 	private int credits;
 
-	public Teacher(int age, String name, int credits) {
+	public Teacher(String name, int age, int credits) {
 		super(age, name);
 		this.credits = credits;
 	}
